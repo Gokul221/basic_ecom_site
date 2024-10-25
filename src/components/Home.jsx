82520af659b5a4c4b0865835e6cb0874fa2e4263
Gokul Bhoi <gokulb0702@gmail.com>
2024-10-26 00:27:11 +0530

    handle async fetch logic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,54 +1,58 @@
 import { useDispatch, useSelector } from "react-redux";
-import productList from "../data/productList.json";
 import cartSlice from "../data/cartSlice";
+import { fetchAllProducts } from "../data/productSlice";
 import "../styles/home.scss";
+import { useEffect } from "react";
 
 const Home = () => {
   const { cartProductIds } = useSelector((state) => state.cart);
+  const { data } = useSelector((state) => state.products);
   const { addToCart, removeFromCart } = cartSlice.actions;
   const dispatch = useDispatch();
 
-  console.log(cartProductIds);
-  
+  useEffect(() => {
+    dispatch(fetchAllProducts("http://localhost:8080/products"));
+  }, [dispatch]);
 
   return (
     <div className="container product-catalogue">
       <div className="row">
-        {productList.products.map((product) => {
-          return (
-            <div className="wrapper col-md-4" key={product.id}>
-              <div className="card">
-                <img
-                  className="card-img-top center-block"
-                  src={product.imageUrl}
-                  alt="Card cap"
-                />
+        {data &&
+          data.map((product) => {
+            return (
+              <div className="wrapper col-md-4" key={product.id}>
+                <div className="card">
+                  <img
+                    className="card-img-top center-block"
+                    src={product.imageUrl}
+                    alt="Card cap"
+                  />
 
-                <div className="card-body text-center">
-                  <h5 className="card-title">{product.name}</h5>
-                  <p className="card-text">${product.price}</p>
+                  <div className="card-body text-center">
+                    <h5 className="card-title">{product.name}</h5>
+                    <p className="card-text">${product.price}</p>
 
-                  {!cartProductIds.includes(product.id) && (
-                    <button
-                      className="btn btn-primary"
-                      onClick={() => dispatch(addToCart(product.id))}
-                    >
-                      Add to cart
-                    </button>
-                  )}
-                  {cartProductIds.includes(product.id) && (
-                    <button
-                      className="btn btn-primary"
-                      onClick={() => dispatch(removeFromCart(product.id))}
-                    >
-                      Remove from cart
-                    </button>
-                  )}
+                    {!cartProductIds.includes(product.id) && (
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => dispatch(addToCart(product.id))}
+                      >
+                        Add to cart
+                      </button>
+                    )}
+                    {cartProductIds.includes(product.id) && (
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => dispatch(removeFromCart(product.id))}
+                      >
+                        Remove from cart
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
